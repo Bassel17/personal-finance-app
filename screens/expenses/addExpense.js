27e142e0ab07542bba9bc82cf5bacd7ac7e9c1de
Bassel Kanso <basselkanso82@gmail.com>
2021-04-14 12:20:31 +0300

    feature: create addExpense screen and add expenses to the local database

--- screens/expenses/addExpense.js
+++ screens/expenses/addExpense.js
@@ -1,15 +1,76 @@
-import React from 'react';
-import { Title } from 'react-native-paper';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import { Title, TextInput, Menu, Button ,Provider} from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import {addExpense} from '../../Database/Account';
 
-const AddExpense = () => (
-    <View style={{ 
-        flex: 1, 
-        backgroundColor: '#673ab7',
-        alignItems:"center" 
-    }} >
-      <Title style={{color:"#ffffff"}}>Add Expense</Title>
-    </View>
-);
+const AddExpense = ({navigation}) => {
+
+  const [description,setDescription] = useState("");
+  const [amount, setAmount] = useState("0");
+  const [currency,setCurrency] = useState("LBP");
+  const [visible,setVisible] = useState(false);
+
+  const add = () => {
+    addExpense({
+      amount:parseInt(amount),
+      currency,
+      description
+    });
+    navigation.goBack();
+  }
+
+
+  return (
+    <Provider>
+      <View style={styles.container}>
+        <TextInput
+          label="Description"
+          value={description}
+          onChangeText={setDescription}
+        />
+        <View style={styles.column}>
+          <TextInput
+            label="Amount"
+            value={amount}
+            onChangeText={setAmount}
+            keyboardType="number-pad"
+            style={styles.input}
+          />
+          <Menu
+            visible={visible}
+            onDismiss={()=>setVisible(false)}
+            anchor={<Button onPress={()=>setVisible(true)}>{currency}</Button>}>
+            <Menu.Item onPress={() => {
+              setCurrency("LBP")
+              setVisible(false)
+            }} title="LBP" />
+            <Menu.Item onPress={() => {
+              setCurrency("USD")
+              setVisible(false)
+            }} title="USD" />
+          </Menu>
+        </View>
+        <Button onPress={add}>Add Expense</Button>
+      </View>
+    </Provider>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container:{
+    padding:10,
+    flexDirection:"column",
+  },
+
+  column:{
+    flexDirection:"row",
+    justifyContent:"space-between"
+  },
+
+  input:{
+    width:"80%"
+  }
+});
 
 export default AddExpense;
--- screens/expenses/index.js
+++ screens/expenses/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import { Title, Button } from 'react-native-paper';
 import { View } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
+import {retrieveAll} from '../../Database/Account';
 
-const Expenses = ({navigation}) => (
+const Expenses = ({navigation}) =>{
+  const [data,setData] = useState([]);
+  const isFocused = useIsFocused();
+
+  useEffect(()=>{
+    setData(retrieveAll('Expense'));
+  },[isFocused])
+
+  if(data[0] == undefined){
+    return (<View></View>)
+  }
+
+  return(
     <View style={{ 
         flex: 1, backgroundColor: '#000000',
         alignItems:"center" 
@@ -10,6 +24,7 @@ const Expenses = ({navigation}) => (
       <Title style={{color:"#ffffff"}}>Expenses</Title>
       <Button onPress={()=>navigation.navigate("AddExpense")}>Add Expense</Button>
     </View>
-);
+  );
+}
 
 export default Expenses;
